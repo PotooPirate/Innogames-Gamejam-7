f0ee6f9884d9e513feb4d25505128759df927841
Joschka Schulz <Horstyschulz@gmail.com>
2014-11-14 19:14:28 +0100

    initial commit

---
+++ desktop/src/de/potoopirate/alf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.potoopirate.alf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.potoopirate.alf.ALFMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ALFMain(), config);
+	}
+}
