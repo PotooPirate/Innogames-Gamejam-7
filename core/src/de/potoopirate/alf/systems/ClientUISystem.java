f9e0e84a53871961e2e09a502f421f3206080036
Joschka Schulz <Horstyschulz@gmail.com>
2014-11-15 04:33:44 +0100

    client ready without graphics

--- core/src/de/potoopirate/alf/systems/ClientUISystem.java
+++ core/src/de/potoopirate/alf/systems/ClientUISystem.java
@@ -3,24 +3,33 @@ package de.potoopirate.alf.systems;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import de.potoopirate.alf.interfaces.IClientSystem;
 
 public class ClientUISystem extends EntitySystem {
-
+	
+	private static final int BUTTON_WIDTH = Gdx.graphics.getWidth()/5;
+	private static final int BUTTON_HEIGHT = Gdx.graphics.getHeight()/4;
+	
 	private Stage stage;
 	private IClientSystem clientSystem;
 	private int activePath;
+	private int x, y;
+	private boolean touched;
 	
 	private ShapeRenderer debugRenderer;
 	
 	public ClientUISystem(IClientSystem clientSystem) {
 		this.clientSystem = clientSystem;
 		debugRenderer = new ShapeRenderer();
+		activePath = 2;
 	}
 	
 	@Override
@@ -31,10 +40,42 @@ public class ClientUISystem extends EntitySystem {
 	    Gdx.input.setInputProcessor(stage);
 	}
 
-	private int x, y;
-	private boolean touched;
-	private static final int BUTTON_WIDTH = Gdx.graphics.getWidth()/5;
-	private static final int BUTTON_HEIGHT = Gdx.graphics.getHeight()/4;
+	
+	private void throwSlot() {
+		if(Gdx.input.isTouched() && !touched) {
+	    	y = 0;
+	    	touched = true;
+	    } else if (Gdx.input.isTouched() && touched) {
+	    	y += Gdx.input.getDeltaY();
+	    } else if (!Gdx.input.isTouched() && touched) {
+	    	touched = false;
+	    	if(y <= -200) {
+	    		x = Gdx.input.getX();
+	    		if (x < Gdx.graphics.getWidth()/3) {
+	    			clientSystem.throwSlot(1, activePath);
+	    		} else if (x < (Gdx.graphics.getWidth()/3)*2) { 
+	    			clientSystem.throwSlot(2, activePath);
+	    		} else {
+	    			clientSystem.throwSlot(3, activePath);
+	    		}
+	    	}
+	    	System.out.println("delta X: " + x + "/ delta Y:" + y);
+	    }
+	}
+	
+	private void changePath() {
+		if(Gdx.input.isTouched()) {
+			int x = Gdx.input.getX();
+			int y = Gdx.input.getY();
+			if (x > BUTTON_WIDTH && x < BUTTON_WIDTH*2 && y < BUTTON_HEIGHT*3) {
+				activePath = 1;
+			} else if (x > BUTTON_WIDTH*2 && x < BUTTON_WIDTH*3 && y < BUTTON_HEIGHT*3 ) {
+				activePath = 2;
+			} else if (x > BUTTON_WIDTH*3 && x < BUTTON_WIDTH*4 && y < BUTTON_HEIGHT*3) {
+				activePath = 3;
+			}
+		}
+	}
 	
 	@Override
 	public void update(float deltaTime) {
@@ -48,31 +89,18 @@ public class ClientUISystem extends EntitySystem {
 	    debugRenderer.setColor(1f, 0, 0, 1);
 	    debugRenderer.line(Gdx.graphics.getWidth()/3, 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight());
 	    debugRenderer.line((Gdx.graphics.getWidth()/3)*2, 0, (Gdx.graphics.getWidth()/3)*2, Gdx.graphics.getHeight());
-	    
-	    debugRenderer.setColor(0, 1f, 0, 1);
-	    debugRenderer.rect(BUTTON_WIDTH, BUTTON_HEIGHT*5, BUTTON_WIDTH, BUTTON_HEIGHT);
-	    debugRenderer.rect(BUTTON_WIDTH*2, BUTTON_HEIGHT*5, BUTTON_WIDTH, BUTTON_HEIGHT);
-	    debugRenderer.rect(BUTTON_WIDTH*3, BUTTON_HEIGHT*5, BUTTON_WIDTH, BUTTON_HEIGHT);
+	    debugRenderer.end();
+
+	    debugRenderer.begin(ShapeType.Filled);
+	    debugRenderer.setColor(activePath == 1 ? Color.GREEN : Color.GRAY);
+	    debugRenderer.rect(BUTTON_WIDTH, BUTTON_HEIGHT*3, BUTTON_WIDTH, BUTTON_HEIGHT);
+	    debugRenderer.setColor(activePath == 2 ? Color.GREEN : Color.GRAY);
+	    debugRenderer.rect(BUTTON_WIDTH*2, BUTTON_HEIGHT*3, BUTTON_WIDTH, BUTTON_HEIGHT);
+	    debugRenderer.setColor(activePath == 3 ? Color.GREEN : Color.GRAY);
+	    debugRenderer.rect(BUTTON_WIDTH*3, BUTTON_HEIGHT*3, BUTTON_WIDTH, BUTTON_HEIGHT);
 	    debugRenderer.end();
 	    
-	    if(Gdx.input.isTouched() && !touched) {
-	    	y = 0;
-	    	touched = true;
-	    } else if (Gdx.input.isTouched() && touched) {
-	    	y += Gdx.input.getDeltaY();
-	    } else if (!Gdx.input.isTouched() && touched) {
-	    	touched = false;
-	    	if(y <= -200) {
-	    		x = Gdx.input.getX();
-	    		if (x < Gdx.graphics.getWidth()/3) {
-	    			clientSystem.throwSlot(1, 1);
-	    		} else if (x < (Gdx.graphics.getWidth()/3)*2) { 
-	    			clientSystem.throwSlot(2, 1);
-	    		} else {
-	    			clientSystem.throwSlot(3, 1);
-	    		}
-	    	}
-	    	System.out.println("delta X: " + x + "/ delta Y:" + y);
-	    }
+	    throwSlot();
+	    changePath();
 	}
 }
