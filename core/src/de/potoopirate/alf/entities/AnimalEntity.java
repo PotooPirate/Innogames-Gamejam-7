8690af902aecb8ddcf1ce5b86cbdf28c14c06c48
Joschka Schulz <Horstyschulz@gmail.com>
2014-11-15 10:49:33 +0100

    rename animal to animalenitity

--- core/src/de/potoopirate/alf/entities/AnimalEntity.java
+++ core/src/de/potoopirate/alf/entities/AnimalEntity.java
@@ -7,7 +7,6 @@ import de.potoopirate.alf.components.PlayerComponent;
 import de.potoopirate.alf.components.RaceComponent;
 
 public class AnimalEntity extends Entity {
-	
 	public AnimalEntity(int playerId) {
 		add(new RaceComponent());
 		add(new PathComponent());
