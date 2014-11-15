bf870e09df380329de1c2e9dde72068f50c34784
Joschka Schulz <Horstyschulz@gmail.com>
2014-11-15 09:22:54 +0100

    removed unused imports

--- core/src/de/potoopirate/alf/systems/ClientUISystem.java
+++ core/src/de/potoopirate/alf/systems/ClientUISystem.java
@@ -7,8 +7,6 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Intersector;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import de.potoopirate.alf.interfaces.IClientSystem;
