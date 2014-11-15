4f9225a236a44ce504ea3b422d3f3baddec47557
CarolaC <carolachristoffel@gmx.de>
2014-11-15 11:18:33 +0100

    added enum Race

--- core/src/de/potoopirate/alf/components/RaceComponent.java
+++ core/src/de/potoopirate/alf/components/RaceComponent.java
@@ -3,5 +3,16 @@ package de.potoopirate.alf.components;
 import com.badlogic.ashley.core.Component;
 
 public class RaceComponent extends Component {
-
+	enum Race {
+		SNAIL, 
+		OCTO, 
+		HIPPO
+	}
+	
+	private Race race;
+	
+	public RaceComponent (Race race) {
+		this.race = race;
+	}
+	
 }
