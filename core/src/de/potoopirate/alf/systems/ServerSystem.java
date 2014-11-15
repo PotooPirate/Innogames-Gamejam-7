639eed640a7567e5bc4a43fbe6bd0a7ab1af0e77
Nidal Osman <nidalo86@yahoo.com>
2014-11-15 21:48:15 +0100

    path change

--- core/src/de/potoopirate/alf/systems/ServerSystem.java
+++ core/src/de/potoopirate/alf/systems/ServerSystem.java
@@ -60,7 +60,7 @@ public class ServerSystem extends EntitySystem {
 		
 		private void handleNetworkMessages(Connection connection, NetworkMessage networkMessage) {
 			
-			if (connection.getID() == 2){
+			if (connection.getID() == 1){
 				if (networkMessage.pathType == 1)
 					networkMessage.pathType = 3;
 				if (networkMessage.pathType == 3)
