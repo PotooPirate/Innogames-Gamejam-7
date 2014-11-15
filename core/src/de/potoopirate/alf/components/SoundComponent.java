388e59f3d0869da8842bde45161998408bdc514d
CarolaC <carolachristoffel@gmx.de>
2014-11-15 13:09:32 +0100

    SoundComponent extends Component

--- core/src/de/potoopirate/alf/components/SoundComponent.java
+++ core/src/de/potoopirate/alf/components/SoundComponent.java
@@ -1,12 +1,14 @@
 package de.potoopirate.alf.components;
 
+import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 
-public class SoundComponent {
+public class SoundComponent extends Component {
 
 	public static final String SNAIL_SOUND = "snail.wav";
-	public static final String OCTOPUS_SOUND = "octopus.wav";
+	public static final String OCTO_SOUND = "octo.wav";
+	public static final String HIPPO_SOUND = "hippo.wav";
 	public Sound animalSound;
 	
 	public SoundComponent (String fileName) {
