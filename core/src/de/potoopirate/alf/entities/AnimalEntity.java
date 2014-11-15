84612a5badb5123360e59a0c6c73709597cdb9c7
CarolaC <carolachristoffel@gmx.de>
2014-11-15 12:57:18 +0100

    added SoundComponent to AnimalEntity

--- core/src/de/potoopirate/alf/entities/AnimalEntity.java
+++ core/src/de/potoopirate/alf/entities/AnimalEntity.java
@@ -6,26 +6,27 @@ import de.potoopirate.alf.components.PathComponent;
 import de.potoopirate.alf.components.PlayerComponent;
 import de.potoopirate.alf.components.RaceComponent;
 import de.potoopirate.alf.components.RaceComponent.Race;
+import de.potoopirate.alf.components.SoundComponent;
 import de.potoopirate.alf.components.TransformComponent;
 
 public class AnimalEntity extends Entity {
-	public AnimalEntity(int playerId, Race race) {
+	public AnimalEntity(int playerId, Race race, String sound) {
 		add(new RaceComponent(race));
 		add(new PathComponent());
 		add(new PlayerComponent(playerId));
-		TransformComponent transform = new TransformComponent();
-		add(transform);
+		add(new TransformComponent());
+		add(new SoundComponent(sound));
 	}
 	
 	public static AnimalEntity createSnail(int playerId) {
-		return new AnimalEntity(playerId, Race.SNAIL);
+		return new AnimalEntity(playerId, Race.SNAIL, SoundComponent.SNAIL_SOUND);
 	}
 
 	public static AnimalEntity createHippo(int playerId) {
-		return new AnimalEntity(playerId, Race.HIPPO);
+		return new AnimalEntity(playerId, Race.HIPPO, SoundComponent.HIPPO_SOUND);
 	}
 
 	public static AnimalEntity createOcto(int playerId) {
-		return new AnimalEntity(playerId, Race.OCTO);
+		return new AnimalEntity(playerId, Race.OCTO, SoundComponent.OCTO_SOUND);
 	}
 }
